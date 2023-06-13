2d0c92ff0d697ab0d6e416651c7e7a09a932f34c
sndwvv <songparkhk@gmail.com>
2023-06-13 15:32:36 +0900

    Initial Commit

---
+++ TypingAnimationSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TypingAnimationSwiftUI
+//
+//  Created by Songyee Park on 2023/06/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TypingAnimationSwiftUI/TypingAnimationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypingAnimationSwiftUIApp.swift
+//  TypingAnimationSwiftUI
+//
+//  Created by Songyee Park on 2023/06/13.
+//
+
+import SwiftUI
+
+@main
+struct TypingAnimationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
